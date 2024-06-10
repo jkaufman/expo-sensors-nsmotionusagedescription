c315e6763e800c785b701ab8d54f97e67bf7ce24
Justin Kaufman <57186+jkaufman@users.noreply.github.com>
2024-06-10 09:53:09 -0600

    Sample.

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { DeviceMotion } from 'expo-sensors'
+import { useEffect } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sample NSMotionUsageDescription App!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+useEffect(() => {
+  DeviceMotion.addListener((motion) => {
+    console.log(JSON.stringify(motion))
+  })
+}, [])
 
 const styles = StyleSheet.create({
   container: {
